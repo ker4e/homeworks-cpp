7758e53186bd96bf154c4969fa50947ca62b264a
ker4e <rus.shirokov.00@bk.ru>
2025-10-13 21:20:38 +0300

    Update 1.2.1.cpp

--- 1.2.1.cpp
+++ 1.2.1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+int main() {
+    int number{};
+    std::cout << "Enter your number: ";
+    std::cin >> number;
+    std::cout << "Number is: " << number << std::endl;
+
+    return 0;
+}
+
+//Для русской версии
+/* #include <iostream>
+#include <windows.h>
+
+int main() {
+    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int number{};
+    std::cout << "Введите ваше число: ";
+    std::cin >> number; 
+    std::cout << "Ваше число: " << number << std::endl;
+
+    return 0;
+}
+*/
