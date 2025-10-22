ad44a6a3f938718634922ea52f29fdcf2f396c83
ker4e <rus.shirokov.00@bk.ru>
2025-10-22 22:08:51 +0300

    Update 1.3.2.cpp

--- 1.3.2.cpp
+++ 1.3.2.cpp
@@ -9,7 +9,7 @@ std::cin >> a >> b >> c;
 int min{}, mid{}, max{};
 max = (a >= c && a >= b) ? a : ((b >= a && b >= c) ? b : c);
 min = (a <= c && a <= b) ? a : ((b <= a && b <= c) ? b : c);
-mid = (a != min && a != max) ? a : ((b != min && b != max) ? b : c);
+mid = a + b + c - min - max;
 
 std::cout << "Numbers in descending order: " << max << " " << mid << " " << min << "\n";
 return 0;
