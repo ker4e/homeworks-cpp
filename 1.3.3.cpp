45da3064c86ba7371ada6b3cd0c08cd8b240a8a9
ker4e <rus.shirokov.00@bk.ru>
2025-10-22 22:11:26 +0300

    Update 1.3.3.cpp

--- 1.3.3.cpp
+++ 1.3.3.cpp
@@ -14,18 +14,16 @@ int main() {
     std::cout << "Enter your age: ";
     std::cin >> age;
 
-    if ((gender == "m" ? 1 : (gender == "male" ? 1 : 0)) ? 
-        ((zodiac == "cancer" ? 1 : (zodiac == "scorpio" ? 1 : (zodiac == "pisces" ? 1 : 0))) ? 
-            (age < 40 ? 1 : 0) 
-        : 0) 
-    : 0) {
+    if ((gender == "m" || gender == "male") &&
+        (zodiac == "cancer" || zodiac == "scorpio" || zodiac == "pisces") &&
+        age < 40)
+    {
         std::cout << "\nToday is a very productive day. You may achieve what once seemed almost impossible.\n";
     }
-    else if ((gender == "f" ? 1 : (gender == "female" ? 1 : 0)) ?
-             ((zodiac == "taurus" ? 1 : (zodiac == "virgo" ? 1 : (zodiac == "capricorn" ? 1 : 0))) ?
-              ((age >= 15 ? (age <= 30 ? 1 : 0) : 0))
-             : 0)
-            : 0) {
+    else if ((gender == "f" || gender == "female") &&
+             (zodiac == "taurus" || zodiac == "virgo" || zodiac == "capricorn") &&
+             (age >= 15 && age <= 30))
+    {
         std::cout << "\nTonight is perfect for socializing with friends, hosting home parties, or spontaneous gatherings.\n"
                      "It will not only be fun, but also engaging: something exciting will catch everyone's interest.\n";
     }
