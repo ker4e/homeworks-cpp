4fabacbd21cc25f5e5e6d6b2cf6958023f801a13
ker4e <rus.shirokov.00@bk.ru>
2025-10-27 20:52:51 +0300

    Update 1.4.1.cpp

--- 1.4.1.cpp
+++ 1.4.1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+int main() {
+    int number{};
+    int sum{0};
+
+    do {
+        std::cout << "Enter an integer or '0' to finish: " << std::endl;
+        std::cin >> number;
+
+        sum += number;
+    } while (number != 0);
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
+
 
