589d4679b81c12aea321622fdadf8a6adf2da8cc
ker4e <rus.shirokov.00@bk.ru>
2025-10-27 21:03:19 +0300

    Update 1.4.2.cpp

--- 1.4.2.cpp
+++ 1.4.2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+
+int main() {
+    int number{};
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    int sum{0};
+
+    if (number < 0) {
+        number = -number;
+    }
+
+    while (number > 0) {
+        sum += number % 10;
+        number /= 10;    
+    }
+
+    std::cout << "Sum of digits: " << sum << std::endl;
+
+    return 0;
+}
+
 
