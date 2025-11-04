bc579fba14a9aed3b989455c719ae47137798d05
ker4e <rus.shirokov.00@bk.ru>
2025-11-04 20:56:17 +0300

    Update 1.5.1.cpp

--- 1.5.1.cpp
+++ 1.5.1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main() {
+    int integers[10] {20, 26, 71, 80, 90, 10, 23, 20, 60, 73};
+
+    for (int row = 0; row < 10; row++) {
+        if (row != 9) {
+            std::cout << integers[row] << ", ";
+        } else {
+            std::cout << integers[row];
+        }
+    }
+  
+    return 0;
+}
