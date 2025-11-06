944a784cb93fea70eff0745119744db44c2427c1
ker4e <rus.shirokov.00@bk.ru>
2025-11-06 10:27:31 +0300

    Implement basic arithmetic operations in C++

---
+++ 1.6.1.1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int twoSum ( int first, int second );
+int twoDiff ( int first, int second );
+int twoMultiplication ( int first, int second );
+int twoDivision ( int first, int second );
+
+int main() {
+  int first {5};
+  int second {10};
+
+  twoSum (first, second);
+  twoDiff(first, second);
+  twoMultiplication (first, second);
+  twoDivision (first, second);
+  
+return 0;
+}
+
+int twoSum ( int first, int second ) 
+{
+int sum{ first + second };
+  std::cout << first << " + " << second << " = " << sum << std::endl;
+  return sum;
+}
+
+int twoDiff ( int first, int second ) 
+{
+int diff{ first - second };
+  std::cout << first << " - " << second << " = " << diff << std::endl;
+  return diff;
+}
+
+int twoMultiplication ( int first, int second )
+{
+int mult { first * second };
+  std::cout << first << " * " << second << " = " << mult << std::endl;
+  return mult;
+}
+
+int twoDivision ( int first, int second )
+{
+int division { first / second };
+  std::cout << first << " / " << second << " = " << division << std::endl;
+  return division;
+}
